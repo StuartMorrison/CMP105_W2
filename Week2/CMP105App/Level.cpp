70b23a0420cc1ec865f8979e49f4cf1ccd8d626e
1900407 <1900407@uad.ac.uk>
2020-01-23 10:57:25 +0000

    I worked through questions 1 to 3 and they worked fine.

--- Week2/CMP105App/Level.cpp
+++ Week2/CMP105App/Level.cpp
@@ -1,5 +1,6 @@
 #include "Level.h"
 
+
 Level::Level(sf::RenderWindow* hwnd, Input* in)
 {
 	window = hwnd;
@@ -17,8 +18,30 @@ Level::~Level()
 // handle user input
 void Level::handleInput()
 {
+	if (input->isKeyDown(sf::Keyboard::W))
+	{
+		input->setKeyUp(sf::Keyboard::W);
+		std::cout << "W was pressed.\n";
+	}
 
+	if (input->isKeyDown(sf::Keyboard::J) && input->isKeyDown(sf::Keyboard::K) && input->isKeyDown(sf::Keyboard::L))
+	{
+		
+		input->setKeyUp(sf::Keyboard::J);
+		input->setKeyUp(sf::Keyboard::K);
+		input->setKeyUp(sf::Keyboard::L);
 
+		std::cout << "J,k and L was pressed.\n";
+
+	}
+
+	if (input->isKeyDown(sf::Keyboard::Escape))
+	{
+		input->setKeyUp(sf::Keyboard::Escape);
+		window->close();
+	}
+
+	
 }
 
 // Update game objects
@@ -30,8 +53,17 @@ void Level::update()
 // Render level
 void Level::render()
 {
+	
 	beginDraw();
-
+	int x = input->getMouseX();
+	int y = input->getMouseY();
+	int test = 0;
+	//std::string output("The X coord is", x);
+	//std::string output2("The Y coord is ", y);
+	std::cout << x;
+	std::cout << y;
+	
+	//std::cout << output;
 	endDraw();
 }
 
